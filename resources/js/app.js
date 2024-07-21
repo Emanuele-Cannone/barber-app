0ca5d8eb221bab5df444381152d76c5529806c63
Emanuele <cannone.emanuele@gmail.com>
2024-07-21 14:25:22 +0200

    add livewire-powergrid package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Clipboard from '@ryangjchandler/alpine-clipboard'
+import Clipboard from '@ryangjchandler/alpine-clipboard';
+
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
+import './../../vendor/power-components/livewire-powergrid/dist/tailwind.css';
 
 Alpine.plugin(Clipboard)
 
